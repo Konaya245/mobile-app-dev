72ae0560d466da50d66f32c0955302b3b197867a
Konaya245 <mzaidi.ar@gmail.com>
2023-10-31 12:51:25 +0800

    sets and maps and functions class

---
+++ codestuff/ca2.dart
@@ -0,0 +1,11 @@
+void main() {
+  var z = const [];
+
+  z.add(12);
+
+  z.add(24);
+
+  z.add(25);
+
+  print("This is the contents of z $z");
+}
--- codestuff/main.dart
+++ codestuff/main.dart
@@ -1,7 +1,40 @@
+//test chamber + notas
 void main() {
-  var numbers = []; //integer values only
-  numbers = [2, 'ali'];
-  numbers.add(2); //insert new value at the end of the list
-  numbers.add('ali'); //insert at position 1, value 4
-  print(numbers.reversed);
-// start next class with no.2 notes part2
+  Map<int, String> fruits = {
+    101: 'apple',
+    102: 'orange',
+    103: 'pear',
+  };
+  fruits[104] = 'watermelon';
+  print(fruits); //print maps
+  fruits.forEach((code, fruit) =>
+      print('$code is ${fruit}')); //print maps with for each loop
+  print(fruits.keys); //print keys
+  fruits.remove(104); //clear map
+  print(fruits.keys); //print keys
+}
+
+/*notes 31/10 
+fixed list using number is kiddy stuff we use generics? unlimited now.
+prefill the list to make a list fixed size
+
+set = unordered object that stores unit values. no duplicates different from array. 
+e.g. matric numbers and names.
+set uses curly braces whereas list uses square brackets. 
+
+must declare variable e.g. <String> infront of the curly braces to make it a set, else it will be a map.
+=> fat arrow function is a shorthand for one line functions.
+arrow function simplest for one line functions when you need to return value.
+
+
+A standard function in dart must consist the following elements:  
+Data_Type Function_Name (parameters){    
+  //Operation on the given parameter;    
+  //Optional Return of the operation’s result; 
+}
+
+square bracket for optional parameters.
+variable assignment in function parameter makes it the default parameter.
+eg. bool threshold(int i =1){}
+
+*/
