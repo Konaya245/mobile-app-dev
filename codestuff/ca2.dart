f593e997cc2b440897548eb4944d2a00899a2d9e
Konaya245 <mzaidi.ar@gmail.com>
2023-11-02 11:52:28 +0800

    class activity 2 wo q5

--- codestuff/ca2.dart
+++ codestuff/ca2.dart
@@ -1,3 +1,4 @@
+/* Q1
 void main() {
   var z = const [];
 
@@ -9,3 +10,66 @@ void main() {
 
   print("This is the contents of z $z");
 }
+*/
+// the error states that the list is constant and cannot be changed
+
+/* Q2
+import 'dart:io';
+void main() {
+  List<String?> fullName = [];
+  bool i = true;
+  while (i) {
+    print("Enter your full name:");
+    fullName.add(stdin.readLineSync());
+
+    print("Is that all?");
+    var answer = stdin.readLineSync();
+    if (answer == "yes") {
+      i = false;
+    }
+  }
+  print(fullName);
+}
+*/
+
+/* Q3
+import 'dart:io';
+
+void main() {
+  Set<String?> fullName = {};
+  bool i = true;
+  while (i) {
+    print("Enter your full name:");
+    fullName.add(stdin.readLineSync());
+
+    print("Is that all?");
+    var answer = stdin.readLineSync();
+    if (answer == "yes") {
+      i = false;
+    }
+  }
+  print(fullName);
+}
+*/
+
+/* Q4
+import 'dart:io';
+
+void main() {
+  Map<int?, String?> fullName = {};
+  bool i = true;
+  
+  while (i) {
+    print("Enter your full name:");
+    var count = fullName.length + 1;
+    fullName[count] = stdin.readLineSync();
+
+    print("Is that all?");
+    var answer = stdin.readLineSync();
+    if (answer == "yes") {
+      i = false;
+    }
+  }
+  fullName.forEach((count, name) => print('$count is ${name}'));
+}
+*/
